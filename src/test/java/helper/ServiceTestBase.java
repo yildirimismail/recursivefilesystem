501d7c9ba70044dac052c98bbf29965f4c0fa436
iyildirim <ismailyildirim07@gmail.com>
2020-07-20 11:32:45 +0100

    Unit Test infrastructure Added Test Cases Added

--- src/test/java/helper/ServiceTestBase.java
+++ src/test/java/helper/ServiceTestBase.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(name = "ipara_test", locations = {"classpath:test-file-system-context.xml"})
+@ContextConfiguration(name = "file_system_test", locations = {"classpath:test-file-system-context.xml"})
 public abstract class ServiceTestBase extends TestCase {
 
     @Before
